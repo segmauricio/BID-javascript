6d9b88d76794a57fb7f6b49409dbff851b77694d
segmauricio <mauribox28@gmail.com>
2023-01-06 14:22:37 -0300

    Practice assignment from Thursday of Week 1 of the MERN module

---
+++ 5. Ninja/ninja.js
@@ -0,0 +1,23 @@
+class Ninja{
+    constructor(name, health, speed, strength){
+        this.name = name;
+        this.health = health || 0;
+        this.speed = speed || 3;
+        this.strength = strength || 3;
+    }
+    sayName(){
+        console.log(`- The name of the ninja is ${this.name}`)
+    }
+    showStats(){
+        console.log(`- The stats of the ninja are the following: Name [${this.name}], Strenght [${this.strength}], Speed [${this.speed}] and Health [${this.health}]`)
+    }
+    drinkSake(){
+        this.health += 10;
+        console.log(`- The stats of the ninja are the following: Name [${this.name}], Strenght [${this.strength}], Speed [${this.speed}] and Health [${this.health}]`)
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa", 7, 4, 5);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
